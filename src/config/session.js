4179d7cf4a5148c8632c1e4bd5afc80887399292
Danarquio <danarquio@outlook.com>
2024-02-08 10:22:30 -0300

    Proyecto final 3.3

--- src/config/session.js
+++ src/config/session.js
@@ -1,11 +1,12 @@
 import session from 'express-session';
-import mongoose from 'mongoose';
+import MongoStore from 'connect-mongo'
+
 
 const sessionConfig = {
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  store: new session.MemoryStore(), 
+  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
   cookie: {
   maxAge: 60 * 60 * 1000 // Establece la duración de la sesión a 24 horas
   }
